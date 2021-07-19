55ab7a14834a07c075bf61c85800451bc1d854dd
Ivan Pavlov <pavlov200912@gmail.com>
2021-07-19 14:28:03 +0300

    unused file HeadlessConfig.kt deleted

--- comment-updater-headless/src/main/kotlin/HeadlessConfig.kt
+++
@@ -1,4 +0,0 @@
-object HeadlessConfig {
-    val OUTPUT_DIR_PATH = "/Users/Ivan.Pavlov/IdeaProjects/CommentUpdater1/dataset"
-    val INPUT_FILE = "/Users/Ivan.Pavlov/IdeaProjects/CommentUpdater1/input.txt"
-}
