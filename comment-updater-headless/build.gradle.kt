b4aaaf5bdb10ca8d0e1de1de798a9ff4e0fbc5b4
Ivan Pavlov <pavlov200912@gmail.com>
2021-08-18 12:38:43 +0300

    headless moved to kts build file

---
+++ comment-updater-headless/build.gradle.kt
@@ -0,0 +1,44 @@
+group = rootProject.group
+version = rootProject.version
+
+dependencies {
+    implementation(project(":comment-updater-core"))
+    implementation("com.beust:klaxon:5.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+    implementation("com.github.ajalt:clikt:2.8.0")
+    implementation("com.google.code.gson:gson:2.6.2")
+}
+
+open class IOCliTask : org.jetbrains.intellij.tasks.RunIdeTask() {
+    @get:Input
+    val runner: String? by project
+    val dataset: String? by project
+    val output: String? by project
+    val config: String? by project
+    val statsOutput: String? by project
+
+    init {
+        jvmArgs = listOf(
+            "-Djava.awt.headless=true",
+            "--add-exports",
+            "java.base/jdk.internal.vm=ALL-UNNAMED",
+            "-Djdk.module.illegalAccess.silent=true"
+        )
+        maxHeapSize = "20g"
+        standardInput = System.`in`
+        standardOutput = System.`out`
+    }
+}
+
+tasks {
+    register<IOCliTask>("runCommentUpdater") {
+        dependsOn("buildPlugin")
+        args = listOfNotNull(
+            runner,
+            dataset?.let { it },
+            output?.let { it },
+            config?.let { it },
+            statsOutput?.let { it }
+        )
+    }
+}
